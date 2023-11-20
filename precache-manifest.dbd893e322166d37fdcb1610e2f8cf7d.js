5d37ef70ed7a6bbb794db22a0b49a73043dfd9ff
jlhrzzz <1246297148@qq.com>
2023-11-20 21:22:34 +0800

    deploy

---
+++ precache-manifest.dbd893e322166d37fdcb1610e2f8cf7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32240b65811ea073621c",
+    "url": "/moni/css/app.37db4a36.css"
+  },
+  {
+    "revision": "c8c3f8da85580ba93ff07e3cfa069856",
+    "url": "/moni/img/code.c8c3f8da.png"
+  },
+  {
+    "revision": "153ced96e8a64137b2fafd2aea08ed68",
+    "url": "/moni/index.html"
+  },
+  {
+    "revision": "32240b65811ea073621c",
+    "url": "/moni/js/app.9c984323.js"
+  },
+  {
+    "revision": "4e0eab85c4d430dab00e",
+    "url": "/moni/js/chunk-vendors.3c9bb34e.js"
+  },
+  {
+    "revision": "0941344b2a2e665b49c71310caf613a5",
+    "url": "/moni/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moni/robots.txt"
+  }
+]);
